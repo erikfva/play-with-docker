fda787a4ad87d0e27b5e96807829524a03c9865c
Erik F. Vargas Agreda <erik.vargas@yandex.com>
2026-04-15 22:47:13 +0000

    chore: remove redundant assign

--- src/middleware/set-google-credentials.js
+++ src/middleware/set-google-credentials.js
@@ -17,7 +17,7 @@ async function setGoogleCredentials(req, res, next) {
   }
  
   try {
-    const outputPath = await initGoogleCredentialsFromS3IfNeeded(headerGoogleCredentials);
+    await initGoogleCredentialsFromS3IfNeeded(headerGoogleCredentials);
     return next();
   } catch (error) {
     return res.status(500).json({ error: error.message || 'Failed to initialize Google credentials from S3'   });
